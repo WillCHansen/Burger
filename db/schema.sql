cc5820ce83169348cd6b24099a378ec274acc920
William Hansen <William Hansen>
2017-04-24 20:07:56 -0700

    db file changes

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Schema
+CREATE DATABASE burgers_db;
+USE burger_db;
+
+CREATE TABLE burgers
+(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`burger_name` VARCHAR(144) NOT NULL,
+	`devoured` BOOLEAN NOT NULL,
+	`date` TIMESTAMP NOT NULL,
+	PRIMARY KEY (`id`)
+);
