446e416ddbdeb452e691c128c4a8ece98d56813f
Will Hansen <whansen@mlabs.com>
2017-04-25 09:20:42 -0700

    added reference html file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (`burger_name`, `devoured`, `date`) VALUES ("The Mack, Big Devil", 1, '1970-01-01 00:00:01');
-INSERT INTO burgers (`burger_name`, `devoured`, `date`) VALUES ("The Ghetto Burger", 1, '2017-4-23 19:03:55');
-INSERT INTO burgers (`burger_name`, `devoured`, `date`) VALUES ("The Burger", 0, '1980-01-19 03:13:07');
+INSERT INTO burgers (`burger_name`, `devoured`) VALUES ("The Mack, Big Devil", 1);
+INSERT INTO burgers (`burger_name`, `devoured`) VALUES ("The Ghetto Burger", 1);
+INSERT INTO burgers (`burger_name`, `devoured`) VALUES ("The Burger", 0);
